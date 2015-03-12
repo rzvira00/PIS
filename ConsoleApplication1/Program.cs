0bda26659cb47e827be5469061a70cf8a4e3a585
iursic00 <iursic00@fesb.hr>
2015-03-12 11:19:17 +0100

    Update Program.cs
    dushaaaaaaaaaaaaaaaaaaaaaa

--- ConsoleApplication1/Program.cs
+++ ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World");
+            Console.WriteLine("Skolski i marelica");
+            Console.WriteLine("a dushaaaaaaaa");
             Console.Read();
         }
     }
